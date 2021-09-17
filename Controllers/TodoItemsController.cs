4bcd2fb206e0a341b34251fe8d9da3f7692576d9
Rickard Andersson (Devies) <ra-devies@inet.se>
2021-09-17 15:41:31 +0200

    Add editorconfig and run dotnet format

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -1,8 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
 
 namespace TodoApi.Controllers
