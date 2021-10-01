cb6bedfe23ab588a93a82c2ba3c895a401263bb1
Rickard Andersson (Devies) <ra-devies@inet.se>
2021-10-01 09:43:48 +0200

    Add a test project with a stupid test

---
+++ test/TodoApiTest/CreateTodo.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TodoApiTest
+{
+    public class CreateTodo 
+    {
+        [Fact]
+        public void ShouldCreateTodo()
+        {
+            Assert.True(true);
+        }
+    }
+}
